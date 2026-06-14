9e1411cdcf643365b41d451f87eaff6afea19393
Thyago-png <thyagopmtebbz@gmail.com>
2026-06-14 19:36:50 -0300

    feat: adicionando codigo fonte do projeto

---
+++ src/components/textarea/index.tsx
@@ -0,0 +1,5 @@
+import styles from'./styles.module.css';
+import {HTMLProps} from 'react';
+export function Textarea({... rest}: HTMLProps<HTMLTextAreaElement>){
+    return <textarea className={styles.textarea}></textarea>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,70 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import styles from "../../styles/home.module.css";
+import Image from 'next/image'
+import imagem_centro from '../../public/assets/imagem_centro.jpeg'
+import {db} from '../services/firebaseConnection'
+import {
+    collection,
+    getDocs
+} from 'firebase/firestore'
+
+interface HomeProps{
+    posts: number;
+    comments: number;
+}
+
+export default function Home({posts, comments}: HomeProps){
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Tarefas+ | Organize suas Tarefas de forma fácil</title>
+            </Head>
+
+            <main className={styles.main}>
+                <div className={styles.logoContent}>
+                    <Image
+                    className={styles.hero}
+                    alt="logo Tarefas+"
+                    src={imagem_centro}
+                    priority
+                    />
+                </div>
+                <h1 className={styles.title}>
+                    Sistema feito para você organizar <br />
+                    seus estudos e tarefas
+                </h1>
+
+                <div className={styles.infoContent}>
+                    <section className={styles.box}>
+                        <span>{posts} posts</span>
+                    </section>
+                    <section className={styles.box}>
+                        <span>{comments} comentarios</span>
+                    </section>
+                </div>
+            
+            </main> 
+
+           
+
+        </div>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const commentRef = collection(db, "comments")
+    const postRef = collection(db, 'tarefas')
+
+    const commentSnapshot = await getDocs(commentRef)
+    const postSnapshot = await getDocs(postRef);
+
+    return {
+        props:{
+            posts: postSnapshot.size || 0,
+            comments: commentSnapshot.size || 0
+        },
+        revalidate: 60,
+        };
+};
